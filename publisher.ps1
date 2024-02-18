5798f6d1e4133aa70c85f936f44012508c45f02e
Gustavo A <gustavo.atencio@outlook.com>
2024-02-18 16:28:05 -0500

    reuse compiled types

--- publisher.ps1
+++ publisher.ps1
@@ -1,5 +1,6 @@
 $dir_to_publish = './public'
 $package = 'package.json'
+$types_dir = './types'
 $lib_dir = './lib'
 $dist_dir = './dist'
 
@@ -11,10 +12,16 @@ New-Item -ItemType Directory -Force -Path $dir_to_publish
 
 Copy-Item -Path $package -Destination $dir_to_publish
 Copy-Item -Path $lib_dir/* -Destination $dir_to_publish -Recurse
+Copy-Item -Path $types_dir/* -Destination $dir_to_publish -Recurse
+Copy-Item -Path $types_dir/* -Destination $dir_to_publish/cjs -Recurse -Force
+Copy-Item -Path $types_dir/* -Destination $dir_to_publish/mjs -Recurse -Force
+
 Copy-Item -Path $dist_dir -Destination $dir_to_publish/dist -Recurse
+Copy-Item -Path $types_dir/* -Destination $dir_to_publish/dist/cjs -Recurse -Force
+Copy-Item -Path $types_dir/* -Destination $dir_to_publish/dist/mjs -Recurse -Force
 
 Write-Host "Copied files to $dir_to_publish"
 
-Set-Location $dir_to_publish
+#Set-Location $dir_to_publish
 
-pnpm publish
+#pnpm publish
