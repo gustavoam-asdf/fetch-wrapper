62463c8b344791e11adaeb3df0ef914e3642fea0
Gustavo A <gustavo.atencio@outlook.com>
2024-02-18 10:46:22 -0500

    change package dist

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,17 +1,42 @@
-import { defineConfig } from 'tsup'
+import { Options, defineConfig } from 'tsup'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-export default defineConfig({
+const commonOptions: Options = {
 	splitting: true,
 	clean: true,
-	dts: true,
-	format: ['cjs', 'esm'],
+	sourcemap: true,
+	outExtension() {
+		return {
+			js: '.js',
+			dts: '.d.ts'
+		}
+	},
 	minify: isProduction,
 	bundle: isProduction,
+	shims: true,
 	skipNodeModulesBundle: true,
 	watch: !isProduction,
 	target: 'es2022',
-	outDir: isProduction ? 'dist' : 'lib',
 	entry: ['src/**/*.ts'],
-})
+}
+
+export default defineConfig([
+	{
+		...commonOptions,
+		format: ['cjs'],
+		outDir: isProduction ? 'dist/cjs' : 'lib/cjs',
+	},
+	{
+		...commonOptions,
+		format: ['esm'],
+		outDir: isProduction ? 'dist/mjs' : 'lib/mjs',
+	},
+	{
+		...commonOptions,
+		dts: {
+			only: true,
+		},
+		outDir: isProduction ? 'dist' : 'lib',
+	}
+])
