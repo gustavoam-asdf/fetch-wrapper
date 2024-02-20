e45a4c93af3bfc13463f3bca459f7a490c9daf0e
Gustavo A. <gustavo.atencio@outlook.com>
2024-02-20 11:47:53 -0500

    first test

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,5 +1,27 @@
-import { expect, test } from 'vitest'
+import { beforeAll, describe, expect, test } from 'vitest'
 
-test('should pass', () => {
-	expect(1).toBe(2)
+import { GetHttpClient } from '../src/http/GetHttpClient.js'
+import { HttpClientManager } from '../src/HttpClientManager.js'
+
+let httpClient: HttpClientManager
+
+beforeAll(() => {
+	httpClient = new HttpClientManager({
+		baseUrl: 'https://jsonplaceholder.typicode.com',
+		defaultBodyParser(response) {
+			return response.json()
+		},
+		config: {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}
+	})
+})
+
+
+test('should return a response', async () => {
+	const response = await httpClient.get("/todos/1")
+	console.log(response)
+	expect(response).toBeDefined()
 })
